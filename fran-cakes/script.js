be726345a6b0f93414d37d068bf6f72b3222de1f
Marcos Milhomem <marcosapolo511@gmail.com>
2023-09-14 20:24:17 -0300

    fran cakes

--- fran-cakes/script.js
+++ fran-cakes/script.js
@@ -1,6 +1,6 @@
 function sair() {
     var a = window.document.querySelectorAll('.titulo-pag3')
-    if (onmouseout.length == true) {
+    if (onmouseout == true) {
         a.style.transition = '3s'
         a.style.borderTop = '0px'
     }
